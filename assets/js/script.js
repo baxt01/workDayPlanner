f9aa83c8302c69e4d2ee33d1b0f97b77ccbec24e
Michael Baxter <baxt01@hotmail.com>
2024-01-20 21:03:49 +0000

    added a js file and added an array of objects to store the hourly data

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// An array of objects to store the hourly data.
+myDay = [
+    {
+        id: "0",
+        hour: "09",
+        time: "09",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "1",
+        hour: "10",
+        time: "10",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "2",
+        hour: "11",
+        time: "11",
+        meridiem: "am",
+        reminder: ""
+    },
+    {
+        id: "3",
+        hour: "12",
+        time: "12",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "4",
+        hour: "1",
+        time: "1",
+        meridiem: "pm",
+        reminder: ""
+    },
+    {
+        id: "5",
+        hour: "2",
+        time: "2",
+        meridiem: "pm",
+        reminder: ""
+    }
+]
+
