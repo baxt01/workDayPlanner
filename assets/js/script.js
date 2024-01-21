7a64b2f14bed4b8ad015f4694ce4e969ef10ad2f
Michael Baxter <baxt01@hotmail.com>
2024-01-21 11:06:53 +0000

    saving the user input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
     var currentDate = dayjs().format("DD MMMM YYYY");
     var displayDate = document.getElementById("currentDay");
         displayDate.innerHTML = currentDate;
-    var currentHour = dayjs().format(HH);
+    var currentHour = dayjs().format("HH");
 
     // Setting the class for past, present and future on the time-blocks.
     $(".time-div").each(function() {
@@ -22,3 +22,10 @@
         }
     });
 
+    // on click of the save button save to the local storage, and prevent default on the browser.
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        var value = $(this).siblings(".time-block").val();
+        var time = $(this).parent().attr("id").split("-")[1];
+        localStorage.setItem(time,value);
+    });
