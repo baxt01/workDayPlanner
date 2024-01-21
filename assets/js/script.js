6f23d5a1cd13acdca9e1e0dd44974dd9bfa52cc8
Michael Baxter <baxt01@hotmail.com>
2024-01-21 10:43:32 +0000

    Added all the time blocks to the HTML and changed my mind on the style in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,8 @@
-// An array of objects to store the hourly data.
-myDay = [
-    {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "1",
-        time: "1",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "2",
-        time: "2",
-        meridiem: "pm",
-        reminder: ""
-    }
-]
-
 // using dayjs to set the date in the header section.
-function setDateInHeader () {
     var currentDate = dayjs().format("DD MMMM YYYY");
-    $("currentDay").text(currentDate);
-}
+    var displayDate = document.getElementById("currentDay");
+        displayDate.innerHTML = currentDate;
+    var currentHour = dayjs().format(HH);
+
+
 
